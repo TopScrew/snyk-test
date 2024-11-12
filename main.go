7971b2145241b38b9122839f28c4769f6805cebd
TopScrew <332767824@qq.com>
2024-11-12 19:53:46 +0800

    test

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("a12")
-	viper.ConfigFileUsed()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
