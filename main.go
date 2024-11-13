9b104faf0bec506665044db1545ad5dfe1b7c74b
xiabee <yunjie.xiao@pingcap.com.cn>
2024-11-13 13:01:10 +0800

    update b1

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/justinas/nosurf"
-	"net/http"
 )
 
 func main() {
 	fmt.Println(nosurf.CookieName)
 
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
