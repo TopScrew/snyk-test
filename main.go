5644d63e780bda9b8319dc8c12e0757ad5fa03b0
TopScrew <332767824@qq.com>
2024-11-13 17:04:45 +0800

    test

--- main.go
+++ main.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/justinas/nosurf"
-	"net/http"
 )
 
 func main() {
 	fmt.Println("a12")
 
-	r := gin.Default()
-	response := backend.DataResponse{
-		Error: fmt.Errorf("example error"),
-	}
-	fmt.Println("Example response:", response)
 	fmt.Println(nosurf.CookieName)
 	fmt.Println("fffffff")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
 }
