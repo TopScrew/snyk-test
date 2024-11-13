1734856c5a11e256214912ef6e648cf268a8eea2
TopScrew <332767824@qq.com>
2024-11-13 17:18:53 +0800

    test

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/justinas/nosurf"
 )
 
@@ -10,10 +9,6 @@ func main() {
 	fmt.Println("a12")
 
 	fmt.Println(nosurf.CookieName)
-	response := backend.DataResponse{
-		Error: fmt.Errorf("example error"),
-	}
-	fmt.Println("Example response:", response)
 	fmt.Println("fffffff")
 
 }
